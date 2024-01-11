20e3d6821d91ae39af6b9a87b16ce09bf41349dc
CodeSandbox <support@codesandbox.io>
2024-01-11 18:44:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let ano = "ano novo está chegando !!";
+console.log(ano);
+
+let nome = "mariah carey";
+console.log(nome);
+
+const idade = 54;
+console.log(idade);
+
+let _ano = 2023;
+console.log(2023);
+
+let $ano = 2023;
+let $idade = 54;
+console.log($ano - $idade);
+
+let quartafeira = true;
+console.log(typeof quartafeira);
+
+let _idade = 18;
+if ($idade >= 18) {
+  console.log(
+    "voce é maior de idade,poderá curtir o show do roberto carlos com participação especial de mariah carey",
+  );
+} else {
+  console.log("sinto muito,mas assistirá da tv globo");
+}
+
+let aniver = "Abril";
+if (aniver == "agosto" || aniver == "dezembro" || aniver == "junho") {
+  console.log(
+    "nenhuma das opçãoes estão corretas, o mes selecionado foi abril",
+  );
+} else {
+  console.log(
+    "algo de errado não está certo, o mes digitado foi o mes de abril",
+  );
+}
